26066d152e066da426f6fcfd772b667feacc38b3
Hikmal-Yudha <hikmalyudha20@gmail.com>
2025-06-06 19:59:45 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function Football() {
+  const shoot = (a, b) => {
+    alert(b.type);
+  };
+
+  return (
+    <button onClick={(event) => shoot("Goal!", event)}>Take The Shot!</button>
+  );
+}
+
+function MissedGoal() {
+  return <h1>MISSED!</h1>;
+}
+
+function MadeGoal() {
+  return <h1>GOAL!</h1>;
+}
+
+function Goal(props) {
+  const isGoal = props.isGoal;
+  if (isGoal) {
+    return <MadeGoal />;
+  }
+  return <MissedGoal />;
+}
+
+function Garage(props) {
+  const car = props.car;
+  return (
+    <>
+      <h1>Garage</h1>
+      {cars.length > 0 && <h2>You have {cars.length} cars in your garage.</h2>}
+    </>
+  );
+}
+const cars = ["Ford", "BMW", "Audi"];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Football />
+      <Goal isGoal={false} />
+      <Garage cars={cars} />
     </div>
   );
 }
